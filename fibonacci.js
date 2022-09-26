e95a32b0b57d13e786007fe2fb8e0ad86fc88774
SharvezGeter <sharvezgeter@gmail.com>
2022-09-26 16:17:12 -0400

    update

--- fibonacci.js
+++ fibonacci.js
@@ -1,29 +1,16 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
+let n = Number(prompt("Enter a value for 'n': "));
 
-let userInput = Number(prompt("Enter a number: "))
-let num1 = 0
-let num2 = 1
-let num3 = Number()
-let user = 0
+let i = 0
+let num = 1
+let total = 0
+console.log(i)
+let count = 1
 
-while(user < userInput){
-    if(user === 0){
-        console.log()
-    }else if ( user === 1){
-        console.log(1)
-    }else{
-        num3 - num1 + num2
-        console.log(num1 + num2)
-        num1 - num2
-        num2 - num3
-        user++
-    }
-    
+while (count < n) { 
+    console.log(num)
+    total = i + num
+    i = num
+    num = total
+    count++;
 }
-
-//Write a program that prompts the user 
-//for two numberx, x and y. The program should 
-//add one to x until it is divisble by y.
-
-
-
