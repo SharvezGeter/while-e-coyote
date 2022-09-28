8d43f570ae7bf048dc36884dfa3d005661d328f8
SharvezGeter <sharvezgeter@gmail.com>
2022-09-28 10:23:03 -0400

    update

--- make-divisble.js
+++ make-divisble.js
@@ -1,26 +1,12 @@
-const prompt = require('prompt-sync')({sigint: true});
 
+const prompt = require('prompt-sync')({ sigint: true });
 
+let x = Number(prompt("Enter a number for X: "));
+let y = Number(prompt("Enter a number for Y: "));
 
-//Write a program that prompts the user for 
-//two numberx, x and y. The program should add one to x 
-//until it is divisble by y.
+while (x%y !== 0 ) {
 
-
-let user1 = Number(prompt("Enter a number for X as variable: "))
-let user2 = Number(prompt("Enter a number for Y as variable: "))
-let user3 = Number(prompt("Enter"))
-
-output - user1 % user2
-
-while(user3 != 0){
-    user1++
-    console.log(user1)
-    user3 - user1 % user2
+    console.log("Add 1 to X untill its divisible by Y: " + x);
+    x++;
 }
-console.log(`${user1} Divisible by ${user2}`)
-
-
-
-
-
+console.log(x + " is divisible by " + y)
